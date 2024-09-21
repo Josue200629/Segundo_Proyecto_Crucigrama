0adf0885b11604178686040a6e61058622adeadb
Josue200629 <c.salas.2@estudiantec.cr>
2024-09-21 13:15:23 -0600

    Avance inicial

--- Constantes.py
+++ Constantes.py
@@ -1,2 +1,4 @@
+
 Ancho = 800
 Alto = 500
+Coordenadas = (700,600)
--- Segundo_Proyecto.py
+++ Segundo_Proyecto.py
@@ -1,15 +1,18 @@
-import pygame 
+import pygame, sys
 import Constantes
-
 
 pygame.init()
 
-Ventana = pygame.display.set_mode((Constantes.Ancho,Constantes.Alto))
-run = True
-while run == True:
+pygame.display.set_caption("Crucigrama") 
+
+Ventana = pygame.display.set_mode(Constantes.Coordenadas)
+"""Resumen: En esta sección se inicializa, se le heredan coordenadas y se le otorga un nombre a la ventana 
+"""
+
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-pygame.QUIT()
+            print("Se ha cerrado correctamente")
+            sys.exit()
 
-            
